c4332b73359819290daa7aa3eaa90270255874c5
Developer <dev@example.com>
2026-04-09 20:03:10 +0530

    fix

--- run.py
+++ run.py
@@ -1,12 +1,6 @@
-from flask import Flask
 import os
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Working perfectly!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, host='127.0.0.1', port=5000)
